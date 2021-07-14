f08cd2351559660b2e998c940a5f224e56dc21d1
Gabriel Cestaro <gabrielcestaro94@gmail.com>
2021-07-14 11:27:42 -0300

    resolucao do exercicio 6

--- src/06-qual-triangulo.js
+++ src/06-qual-triangulo.js
@@ -12,8 +12,53 @@
  * Caso não seja um triângulo válido, deve retornar 'Invalido'.
  *
  */
-export default function qualTriangulo (lado1, lado2, lado3) {
+export default function qualTriangulo(lado1, lado2, lado3) {
   // Implemente sua solução aqui :)
-  return `Valido-Tipo
-Retangulo: X`
+
+  const validoPorLado1 = Math.abs(lado2 - lado3) < lado1 && lado1 < lado2 + lado3
+  const validoPorLado2 = Math.abs(lado1 - lado3) < lado2 && lado2 < lado1 + lado3
+  const validoPorLado3 = Math.abs(lado1 - lado2) < lado3 && lado3 < lado1 + lado2
+
+  if (!validoPorLado1 && !validoPorLado2 && !validoPorLado3) {
+    return 'Invalido'
+  }
+
+  const tipo = determinarTipo(lado1, lado2, lado3)
+
+  const isRetangulo = determinarRetangulo(lado1, lado2, lado3)
+
+  return `Valido-${tipo}
+Retangulo: ${isRetangulo ? 'S' : 'N'}`
 }
+
+function determinarTipo(lado1, lado2, lado3) {
+  if (lado1 === lado2 && lado2 === lado3) {
+    return 'Equilatero'
+  } else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+    return 'Isoceles'
+  }
+
+  return 'Escaleno'
+}
+
+function determinarRetangulo(lado1, lado2, lado3) {
+
+  if (lado1 > lado2) {
+    if (lado1 > lado3) {
+      return determinarRetanguloPorPitagoras(lado1, lado2, lado3)
+    }
+
+    return determinarRetanguloPorPitagoras(lado3, lado1, lado2)
+  }
+
+  if (lado2 > lado3) {
+    return determinarRetanguloPorPitagoras(lado2, lado1, lado3)
+  }
+
+  return determinarRetanguloPorPitagoras(lado3, lado1, lado2)
+}
+
+function determinarRetanguloPorPitagoras(hipotenusa, cateto1, cateto2) {
+  return Math.pow(hipotenusa, 2) === Math.pow(cateto1, 2) + Math.pow(cateto2, 2)
+}
+
