76124cc145e2853ed8dda85b472774d064d2c7da
Gabriel <59989203+gabrieltrudes@users.noreply.github.com>
2021-08-18 01:50:31 -0300

    Últimos desafios concluídos

--- src/09-notas-e-moedas.js
+++ src/09-notas-e-moedas.js
@@ -22,20 +22,50 @@
  * 0 moeda(s) de R$ 0.05
  * 3 moeda(s) de R$ 0.01
  */
-export default function notasEMoedas (valor) {
-  // Implemente sua solução aqui :)
-  return `NOTAS:
-0 nota(s) de R$ 100.00
-0 nota(s) de R$ 50.00
-0 nota(s) de R$ 20.00
-0 nota(s) de R$ 10.00
-0 nota(s) de R$ 5.00
-0 nota(s) de R$ 2.00
-MOEDAS:
-0 moeda(s) de R$ 1.00
-0 moeda(s) de R$ 0.50
-0 moeda(s) de R$ 0.25
-0 moeda(s) de R$ 0.10
-0 moeda(s) de R$ 0.05
-0 moeda(s) de R$ 0.01`
+export default function notasEMoedas(valor) {
+  const resultado = ['NOTAS:']
+  const cedulasPossiveis = [100, 50, 20, 10, 5, 2]
+
+  let valorParaCalculo = valor
+  for (let i = 0; i < cedulasPossiveis.length; i++) {
+    const cedula = cedulasPossiveis[i]
+
+    const resto = calcularRestoDaDivisao(valorParaCalculo, cedula)
+
+    const quantidadeDeCedulas = dividirInteiros(valorParaCalculo, cedula)
+
+    resultado.push(`${quantidadeDeCedulas} nota(s) de R$ ${cedula}.00`)
+
+    valorParaCalculo = resto
+  }
+
+  resultado.push('MOEDAS:')
+
+  const moedasDeUmReal = dividirInteiros(valorParaCalculo, 1)
+
+  resultado.push(`${moedasDeUmReal} moeda(s) de R$ 1.00`)
+
+  const moedasPossiveis = [50, 25, 10, 5, 1]
+
+  valorParaCalculo = (valor % 1) * 100
+
+  for (let i = 0; i < moedasPossiveis.length; i++) {
+    const moeda = moedasPossiveis[i]
+    const resto = calcularRestoDaDivisao(valorParaCalculo, moeda)
+    const quantidadeDeMoedas = dividirInteiros(valorParaCalculo, moeda)
+
+    resultado.push(`${quantidadeDeMoedas} moeda(s) de R$ ${(moeda / 100).toFixed(2)}`)
+
+    valorParaCalculo = resto
+  }
+
+  return resultado.join('\n')
+}
+
+function calcularRestoDaDivisao(dividendo, divisor) {
+  return dividendo % divisor
+}
+
+function dividirInteiros(dividendo, divisor) {
+  return Math.floor(dividendo / divisor)
 }
